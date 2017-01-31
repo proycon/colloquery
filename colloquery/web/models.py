6d47cde7db79f0c72de1fcbd1eb8c11a45865284
Maarten van Gompel <proycon@anaproy.nl>
2017-01-31 17:39:23 +0100

    model revision

--- colloquery/web/models.py
+++ colloquery/web/models.py
@@ -4,21 +4,26 @@ from django.db import models
 class Collection(models.model):
     collection = models.IntegerField()
     name = models.CharField(max_length=50)
-    language1 = models.CharField(max_length=3)
-    language2 = models.CharField(max_length=3)
+    sourcelanguage = models.CharField(max_length=3)
+    targetlanguage = models.CharField(max_length=3)
 
 class Collocation(models.Model):
     collection = models.IntegerField()
     language = models.CharField(max_length=3)
     text = models.CharField(max_length=125)
     freq = models.IntegerField() #occurrence frequency (absolute)
+    translations = models.ManyToManyField("self", through="Translation", symmetrical=False)
 
     class Meta:
         unique_together = (("collection","language","text"),)
         index_together = (("collection","language","text"),)
 
+class Keyword(models.Model):
+    text = models.CharField(max_length=60)
+    collocations = models.ManyToManyField(Collocation)
+
 class Translation(models.Model):
-    pattern1 = models.ForeignField(Collocation, db_index=True)
-    pattern2 = models.ForeignField(Collocation, db_index=True)
+    source = models.ForeignKey(Collocation, on_delete=models.CASCADE)
+    target = models.ForeignKey(Collocation, on_delete=models.CASCADE)
     prob = models.FloatField() #p(pattern1|pattern2)
     reverseprob = models.FloatField() #p(pattern2|pattern1)
