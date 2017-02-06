15327ee5f49fe4ec78f35808716abc04c37a3387
Maarten van Gompel <proycon@anaproy.nl>
2017-02-06 17:24:57 +0100

    try utf8_bin collation

--- colloquery/web/management/commands/correctcollation.py
+++ colloquery/web/management/commands/correctcollation.py
@@ -10,4 +10,4 @@ class Command(BaseCommand):
         cursor.execute('SHOW TABLES')
         results=[]
         for row in cursor.fetchall(): results.append(row)
-        for row in results: cursor.execute('ALTER TABLE %s CONVERT TO CHARACTER SET utf8 COLLATE     utf8_general_cs;' % (row[0]))
+        for row in results: cursor.execute('ALTER TABLE %s CONVERT TO CHARACTER SET utf8 COLLATE     utf8_bin;' % (row[0]))
